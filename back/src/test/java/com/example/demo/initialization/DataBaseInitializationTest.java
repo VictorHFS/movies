1b5d5ada32d47b024cd4afb1855e5a5282bc5e5a
Victor Soares <victor.soares@elotech.com.br>
2022-12-09 09:46:35 -0300

    Teste

--- back/src/test/java/com/example/demo/initialization/DataBaseInitializationTest.java
+++ back/src/test/java/com/example/demo/initialization/DataBaseInitializationTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.initialization;
 
 import com.example.demo.repository.MovieRepository;
+import com.example.demo.repository.ProducerRepository;
+import com.example.demo.repository.StudioRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,8 +15,16 @@ public class DataBaseInitializationTest {
     @Autowired
     private MovieRepository movieRepository;
 
+    @Autowired
+    private ProducerRepository producerRepository;
+
+    @Autowired
+    private StudioRepository studioRepository;
+
     @Test
     public void test() {
-        assertEquals(206, movieRepository.count());
+VV        assertEquals(206, movieRepository.count());
+        assertEquals(360, producerRepository.count());
+        assertEquals(59, studioRepository.count());
     }
 }
